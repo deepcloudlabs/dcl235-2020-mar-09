55e2d44777c2aab643d1e7d0ee6ac5ab7029ac06
deepcloudlabs <deepcloudlabs@gmail.com>
2020-03-10 14:33:27 +0300

    study hexagonal architecture

---
+++ srp-repository-pattern/src/main/java/com/example/service/BusinessService.java
@@ -0,0 +1,14 @@
+package com.example.service;
+
+import com.example.hr.repository.EmployeeRepository;
+// Hexagonal Architecture (Clean Architecture)
+public class BusinessService {
+	private EmployeeRepository empRepo;
+
+	public BusinessService(EmployeeRepository empRepo) {
+		this.empRepo = empRepo;
+	}
+	
+	public void fun() {}
+	public void gun() {}
+}
