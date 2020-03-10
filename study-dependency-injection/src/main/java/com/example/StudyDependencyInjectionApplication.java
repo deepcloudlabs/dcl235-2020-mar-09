7480f70f8c1462341067508480e6507c841cc01d
deepcloudlabs <deepcloudlabs@gmail.com>
2020-03-10 08:39:47 +0300

    remove unused variable warning

--- study-dependency-injection/src/main/java/com/example/StudyDependencyInjectionApplication.java
+++ study-dependency-injection/src/main/java/com/example/StudyDependencyInjectionApplication.java
@@ -14,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.example.service.Calculator;
 
 // -Dspring.profiles.active=prod
+@SuppressWarnings("unused")
 @SpringBootApplication
 public class StudyDependencyInjectionApplication implements ApplicationRunner {
 	
