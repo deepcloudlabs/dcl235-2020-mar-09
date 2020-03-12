5468cba3456556decad99c6e2993e8986a63c05f
deepcloudlabs <deepcloudlabs@gmail.com>
2020-03-12 09:04:45 +0300

    initial commit

---
+++ stream-api-exercises-part1/src/com/example/exercises/Exercise1.java
@@ -0,0 +1,42 @@
+package com.example.exercises;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import com.example.domain.Director;
+import com.example.domain.Movie;
+import com.example.service.InMemoryMovieService;
+import com.example.service.MovieService;
+
+/**
+ * 
+ * @author Binnur Kurt <binnur.kurt@gmail.com>
+ *
+ */
+public class Exercise1 {
+	private static final MovieService movieService = InMemoryMovieService.getInstance();
+
+	public static void main(String[] args) {
+		// Find the number of movies of each director
+        final Collection<Movie> movies = 
+        		movieService.findAllMovies();
+        System.out.println(movies);
+        Map<Director,Integer> directors = new HashMap<>();
+        // External Loop
+        for (Movie movie : movies) {
+        	for (Director director : movie.getDirectors()) {
+        		Integer numOfMovies = directors.get(director);
+        		if (Objects.isNull(numOfMovies))
+        			numOfMovies = 0;
+        		numOfMovies++;
+        		directors.put(director, numOfMovies);
+        	}
+        }
+        for (Map.Entry<Director, Integer> entry : directors.entrySet())
+        	System.out.println(entry.getKey().getName()+": "+entry.getValue());
+        movies.parallelStream().forEach( movie -> System.out.println(movie));
+	}
+
+}
