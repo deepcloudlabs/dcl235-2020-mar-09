c94ea71764a2ee23b60ea16076e7a37a542615c4
deepcloudlabs <deepcloudlabs@gmail.com>
2020-03-13 15:14:15 +0300

    initial commit

---
+++ async-spring-boot/src/main/java/com/example/AsyncSpringBootApplication.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import java.util.concurrent.Future;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.example.service.BusinessService;
+
+@SpringBootApplication
+@EnableScheduling
+public class AsyncSpringBootApplication implements ApplicationRunner {
+
+	@Autowired
+	private BusinessService businessService;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncSpringBootApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+//		Future<Integer> result = businessService.fun();
+//		while(!result.isDone()) {
+//			System.out.println("Working hard!");
+//		}
+//		System.err.println(result.get());
+	}
+
+}
